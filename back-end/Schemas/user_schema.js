041147143a1ed70773063473374504c33babc1b2
ldhan2110 <ldhan211099@gmail.com>
2021-12-16 17:17:31 +0700

    Update user_schema.js

--- back-end/Schemas/user_schema.js
+++ back-end/Schemas/user_schema.js
@@ -1,14 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    usr_nm:  String, // String is shorthand for {type: String}
-    usr_pwd: String,
-    usr_fullname:   String,
-    usr_dob: String,
-    active: {type: Boolean, default: true},
-    usr_role: String,
-    create_date: { type: Date, default: Date.now },
-    update_date: { type: Date, default: Date.now },
-  });
+const userSchema=new mongoose.Schema({
+    usr_nm:String,
+    usr_pw: String,
+    usr_fullname:String,
+    usr_email:String,
+    usr_addr:String,
+    usr_dob:String,
+    active:{type:Boolean, default: true},
+    otp:{type:String, default:null},
+    created_time_otp: {type:Date, default: null},
+    usr_role: {type:String, default:"bidder"}
+});
 
 export default userSchema;
